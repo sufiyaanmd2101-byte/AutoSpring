b2f173044739a1c17de763b1c657a0107a21924e
Mohammad Sufiyaan <sufiyaan.md2101@gmail.com>
2026-07-17 15:17:36 +0530

    added Dockerfile

---
+++ src/main/java/com/Sufiyaan/AutoSuggest/AutoSuggestApplication.java
@@ -0,0 +1,13 @@
+package com.Sufiyaan.AutoSuggest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoSuggestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoSuggestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Sufiyaan/AutoSuggest/AutoSuggestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Sufiyaan.AutoSuggest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoSuggestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
